6c0c859a59ea577d4a026ac54881f1b491bf9f17
codingchris779 <33784082+codingchris779@users.noreply.github.com>
2017-11-30 13:54:09 -0500

    This is the lazy AI commit

--- test.py
+++ test.py
@@ -1,98 +1,45 @@
-from oWinner import won
+from oWinner import oWin
 from xWinner import xWin
 import multiprocessing
+def chooseRandomMoveFromList(open, movesList):
+    # Returns a valid move from the passed list on the passed board.
+    # Returns None if there is no valid move.
+    possibleMoves = []
+    for i in movesList:
+        if i in open:
+             possibleMoves.append(i)
+    if len(possibleMoves) != 0:
+        return random.choice(possibleMoves)
+    else:
+         return None
 
-xPos = []
-oPos = []
-open = [1,2,3,4,5,6,7,8,9]
-class TicTacToeBrain :
-    xPos = []
-    oPos = []
-    open = [1,2,3,4,5,6,7,8,9]
-    def __init__(self, player = "x") :
-        self._squares = {}
-        self._copySquares = {}
 
-    def getAvailableMoves(self,open) :
-        return open
-
-    def makeMove(self, position, player) :
-        open.remove(position)
-        if player == 'x':
-            xPos.append(position)
+def getComputerMove(open,oPos,xPos):
+    oposcopy=oPos
+    xposcopy=xPos
+    for i in open:
+        oposcopy.append(i)
+        if oWin(oposcopy):
+            return i
         else:
-            oPos.append(position)
-    def undoMove(self, position, player) :
-        open.append(position)
-        if player == 'x':
-            xPos.remove(position)
+            oposcopy.remove(i)
+    for i in open:
+        xposcopy.append(i)
+        if xWin(xposcopy):
+            return i
         else:
-            oPos.remove(position)
-    def complete(self,open) :
-        # *** see above
-        if len(open)==0:
-            return True
-        if self.getWinner(xPos,oPos,open) != None :
-            return True
-        return False
+            xposcopy.remove(i)
 
-    def getWinner(self,xPos,oPos,open) :
-        if won(xPos):
-            return 'x'
-        if won(oPos):
-            return 'o'
-        # *** see above
-        if len(open) == 0:
-            return "tie"
-        return None
-
-    def getEnemyPlayer(self, player) :
-        if player == "x" :
-            return "o"
-        return "x"
-
-    def minimax(self, player,open,xPos,oPos, depth = 0):
-        if player == "o":
-            best = -30
-        else:
-            best = 30
-        if self.complete(open) :
-            if self.getWinner(open,xPos,oPos) == "x" :
-
-                return -30 + depth, None
-            elif self.getWinner(open,xPos,oPos) == "tie" :
-
-                return 0, None
-            elif self.getWinner(open,xPos,oPos) == "o" :
-
-                return 30 - depth, None
-
-        bestMove = None
-        for move in self.getAvailableMoves(open) :
-            self.makeMove(move, player)
-            val, _ = self.minimax(self.getEnemyPlayer(player),open,xPos,oPos, depth+1)
-            #print(val)
-            # *** undo last move
-            self.undoMove(move, player)
-            if player == "o" :
-                if val > best :
-                    # *** Also keep track of the actual move
-                    best, bestMove = val, move
-            else :
-                if val < best :
-                    # *** Also keep track of the actual move
-                    best, bestMove = val, move
-        bestMove = bestMove
-        return best, bestMove
-
-    def printCopy(self) :
-        print(self._copySquares)
-game = TicTacToeBrain()
-# val, bestMove = game.minimax("o",open,xPos,oPos)
-# print ("best move", bestMove )
-while game.complete:
-    userInput = int(input())
-    game.makeMove(userInput, 'x')
-    val, bestMove = game.minimax("o",open,xPos,oPos)
-    game.makeMove(bestMove, 'o')
-    print (bestMove)
+    move = chooseRandomMoveFromList(open, [1, 3, 7, 9,10,19,12,21,16,25,18,27])
+    if move != None:
+            return move
+    else:
+        move = chooseRandomMoveFromList()
+    if move != None:
+        return move
+    else:
+        return chooseRandomMoveFromList(board, [2,11,20,13,22,15,24,17,26,1, 4, 6, 8])
+open=[1,2,3,4,5,7]
+xPos=[6]
+oPos=[8,9]
+print (getComputerMove(open,oPos,xPos))
